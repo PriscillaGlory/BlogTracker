206351199f020cbec2f42eb7b7f1fd8eac64b616
priscilla glory jannu <priscillagloryjannu66@gmail.com>
2024-02-29 20:05:51 +0530

    Add project files.

---
+++ BlogTracker/Data/BlogTrackerDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogTracker.Models;
+
+namespace BlogTracker.Data
+{
+    public class BlogTrackerDbContext : DbContext
+    {
+        public BlogTrackerDbContext (DbContextOptions<BlogTrackerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogTracker.Models.AdminInfo> AdminInfo { get; set; } = default!;
+
+        public DbSet<BlogTracker.Models.BlogInfo>? BlogInfo { get; set; }
+
+        public DbSet<BlogTracker.Models.EmpInfo>? EmpInfo { get; set; }
+    }
+}
